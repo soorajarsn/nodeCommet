6fd0bda8e21819337d093e8f40f14f7d749e2354
Sooraj Shukla <soorajshukla974@gmail.com>
2020-06-29 22:08:07 +0530

    change project structure

---
+++ crawler/build/precache-manifest.c4c6f32bf7510e15674283dc02b0f12d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1d3b83e9c830d5b6d7a581b392802f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f48158a10317ee074fd",
+    "url": "/static/js/2.ed932ab8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed932ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f81b5d8d81a7d6d56e",
+    "url": "/static/js/main.f8b5f9b2.chunk.js"
+  },
+  {
+    "revision": "327b3fc592d05bc276c6",
+    "url": "/static/js/runtime-main.5a3db3ad.js"
+  }
+]);
