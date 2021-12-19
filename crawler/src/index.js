b0d0532adb36db3c6d5600269b6463bca79bc155
Sooraj Shukla <soorajshukla974@gmail.com>
2021-12-19 12:09:38 +0530

    refactor code-base

--- crawler/src/index.js
+++ crawler/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/App';
+import './reset.scss';
+import './theme.scss';
+import './styles.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
